93fc773ff5177217419a7be7bb5c1811ba90013a
nayeon-jo <nyc0516@naver.com>
2023-01-23 15:34:12 +0900

    SessionConst

---
+++ src/main/java/hello/login/domain/login/SessionConst.java
@@ -0,0 +1,8 @@
+package hello.login.domain.login;
+
+public interface SessionConst {
+    //SessionConst.LOGIN_MEMBER는 Key, loginMember는 Value에 해당
+    public static String LOGIN_MEMBER = "loginMember";
+    public static String LOGIN_CUSTOMER="loginCustomer";
+
+}
